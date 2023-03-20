5ba73a22a16d6c7384478becf14e3e5f47288ae3
Deasy Indrawati <indrawatideasy@gmail.com>
2023-03-20 15:33:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ import pandas as pd
 import xgboost as xgb
 from sklearn import datasets
 
+#import the data
+data = pd.read_csv("https://raw.githubusercontent.com/indrawatideasy/profil-pemda/main/pengelolaangup_train.csv")
+
 #Apps Title
 st.write("""
 # UP OPTIMA
 Aplikasi Prediksi Nilai UP Optimal Satker Lingkup Kanwil DJPB Sumsel
 """)
 
-#Subheader
+#input the numbers
+nilai_up = st.slider('UP')
 
+#Subheader
 
 if st.button('Prediksi Realisasi GUP'):
     realgup = predict(UP, PAGU, REALISASI, PAGU52, REAL52, PAGU53, REAL53, STATUS)
