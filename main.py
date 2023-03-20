80302310cf3cc3efb5c113489a2b12f1644e01ab
Deasy Indrawati <indrawatideasy@gmail.com>
2023-03-20 15:37:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,13 @@ Aplikasi Prediksi Nilai UP Optimal Satker Lingkup Kanwil DJPB Sumsel
 """)
 
 #input the numbers
-nilai_up = st.slider('UP')
+nilai_up = st.number_input('UP')
+nilai_pagu = st.number_input('PAGU')
+nilai_real = st.number_input('REALISASI')
+nilai_pagu52 = st.number_input('PAGU52')
+nilai_real52 = st.number_input('REAL52')
+nilai_pagu53 = st.number_input('PAGU53')
+nilai_real53 = st.number_input('REAL53')
 
 #Subheader
 
