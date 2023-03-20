ac6ed528ddf516fb00ead5157f784385a8dbf110
Deasy Indrawati <indrawatideasy@gmail.com>
2023-03-20 09:51:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,20 @@ Aplikasi Prediksi Nilai UP Optimal Satker Lingkup Kanwil DJPB Sumsel
 
 #Sidebar
 st.sidebar.header('Masukkan Parameter')
-st.sidebar.number_input('Insert a number')
 
 def user_input_features():
-    sepal_length = number_input('Insert a number')
+    def user_input_features():
+    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
     data = {'sepal_length': sepal_length,
-            }
+            'sepal_width': sepal_width,
+            'petal_length': petal_length,
+            'petal_width': petal_width}
     features = pd.DataFrame(data, index=[0])
     return features
-
+    
 #User Input Parameter
 df = user_input_features()
 
