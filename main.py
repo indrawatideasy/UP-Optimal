9a3d72f5d2edaa2620269b4b3211b6e71ae37237
Deasy Indrawati <indrawatideasy@gmail.com>
2023-03-21 07:46:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -41,5 +41,5 @@ predictions = [round(value) for value in y_pred]
 
 #checking prediction 
 if st.button("Prediksi"):
-    st.header('Prediksi Realisasi GUP yaitu Rp {}'.format(predictions))
-    st.subheader('Rentang prediksi nilai realisasi GUP yaitu Rp {} sd Rp {}'.format(predictions-errors),(predictions+errors))
+    st.header("Prediksi Realisasi GUP yaitu Rp {}".format(predictions))
+    st.subheader("Rentang prediksi nilai realisasi GUP yaitu Rp {} sd Rp {}".format(predictions-errors),(predictions+errors))
