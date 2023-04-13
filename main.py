d25466cb279fb1846f39dc5e3e9ae8bca7269e81
Deasy Indrawati <indrawatideasy@gmail.com>
2023-04-13 16:09:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ import joblib as joblib
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
-#import the data
-df = pd.read_csv("pengelolaangup.csv")
-model = joblib.load('pred_realgup.pkl')
+#Loading up the Regression model we created
+model = xgb.XGBRegressor()
+model.load_model('xgb_model.json')
+
+#Caching the model for faster loading
+@st.cache
+
+# Define the prediction function
 def predict(UP, PAGU, REALISASI, PAGU52, REAL52):
-	prediction=model.predict([[UP, PAGU, REALISASI, PAGU52, REAL52]])
+	prediction = model.predict(pd.DataFrame([[UP, PAGU, REALISASI, PAGU52, REAL52]], columns=['UP', 'PAGU', 'REALISASI', 'PAGU52', 'REAL52']))
 	return prediction
 
 #Apps Title
@@ -21,14 +26,13 @@ Aplikasi Prediksi Nilai UP Optimal Satker Lingkup Kanwil DJPB Sumsel
 """)
 
 #input the numbers
-df_pred = pd.DataFrame(columns= ['UP', 'PAGU', 'REALISASI', 'PAGU52', 'REAL52'])
-df_pred['UP'] = st.number_input("Nilai UP", float(1), float(100000000000))
-df_pred['PAGU'] = st.number_input("Nilai PAGU", float(1), float(100000000000))
-df_pred['REALISASI'] = st.number_input("Nilai REALISASI", float(1), float(100000000000))
-df_pred['PAGU52'] = st.number_input("Nilai PAGU52", float(1), float(100000000000))
-df_pred['REAL52'] = st.number_input("Nilai REAL52", float(1), float(100000000000))
+UP = st.number_input('UP:', min_value=1.0, max_value=10000000000.0, value=1.0)
+PAGU = st.number_input('PAGU:', min_value=1.0, max_value=10000000000.0, value=1.0)
+REALISASI = st.number_input('REALISASI:', min_value=1.0, max_value=10000000000.0, value=1.0)
+PAGU52 = st.number_input('PAGU52:', min_value=1.0, max_value=10000000000.0, value=1.0)
+REAL52 = st.number_input('REAL52:', min_value=1.0, max_value=10000000000.0, value=1.0)
 
 #checking prediction 
-if st.button("Prediksi"):
-	result=predict('UP', 'PAGU', 'REALISASI', 'PAGU52', 'REAL52')
-	st.success("Prediksi realisasi GUP yaitu{}.".format(result))
+if st.button('Prediksi'):
+    realgup = predict(UP, PAGU, REALISASI, PAGU52, REAL52)
+    st.success(f'The predicted price of the diamond is ${price[0]:.2f} USD')
